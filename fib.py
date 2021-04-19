99c51e87ef5f2511a884301ff324c8fccd729f26
Яковлева Светлана <svetlana1905.iakovleva@yandex.ru>
2021-04-19 15:44:42 +0300

    firsr version

---
+++ fib.py
@@ -0,0 +1,16 @@
+'''Реализовать функцию вычисления i-го числа Фибоначчи.'''
+
+def fib(n):
+    if type(n)!=int:
+        raise TypeError("n must be int")
+    elif n <= 0:
+        raise ValueError("n must be >= 0")
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    return fib(n-1)+fib(n-2)
+
+
+if __name__ == '__main__':
+    print(fib(10))    
